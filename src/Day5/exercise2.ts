3f124b040f555df1ea248e5519d4d516144fc40a
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-01-24 22:44:07 -0300

    Ajustes

--- src/Day5/exercise2.ts
+++ src/Day5/exercise2.ts
@@ -1,14 +1,14 @@
 // Day 5 - Exercise 2
 
-interface Person{
+interface Person1{
     name: string
     age: number
 }
 
-class Person implements Person{
+class Person2 implements Person1{
     constructor(public name: string, public age: number) {}
 }
 
-const jane = new Person('Jane', 31)
+const jane = new Person2('Jane', 31)
 
 console.log(`${jane.name} is ${jane.age} years old`)
---
+++ src/Day6/Day6.ts
@@ -0,0 +1,51 @@
+// Readonly
+class Movies {
+
+    constructor(
+        public readonly id: number,
+        public name: string,
+        private _price: number) { }
+}
+
+let movie1 = new Movies(1, 'Dejavu', 20)
+console.log(movie1)
+
+
+class HotelRooms {
+    [roomNamber: string]: string
+}
+
+let room = new HotelRooms()
+
+room.A201 = 'Marcos'
+room.A202 = 'Julio'
+room.A17 = 'Jose'
+
+// Herança
+
+class Person {
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public age: number
+    ) { }
+
+    get greet(){
+        return this.firstName + ' ' + this.lastName
+    }
+}
+
+class Clients extends Person{
+    override get greet(){
+        return 'Dear ' + super.greet
+    }
+}
+
+class Staff extends Person{
+    override get greet(){
+        return 'Hi ' + super.greet
+    }
+}
+
+let client1 = new Clients('Marcos', 'Fernandes', 22)
+let staff1 = new Staff('Jose', 'Fernandes', 22)
